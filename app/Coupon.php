d40236d3a2854daa4068cbd04bdd3d0ad24adfeb
Sodruldeen Mustapha <sodruldeen.mustapha@gunsel.com.tr>
2019-02-21 08:54:16 +0200

    basic scanned through the models and prevented accidental insertion of id since its on auto increment, just a basic precaution

--- app/Coupon.php
+++ app/Coupon.php
@@ -6,5 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Coupon extends Model
 {
-    //
+    /**
+     * Prevent accidental insertion of id since it is on auto increment.
+     * 
+     */
+    protected $id = ['id'];
 }
